d6bd0a4041f9996feedf0f8ad57e217bbae446b3
Cristhiane Jacques <cjn_1707@hotmail.com>
2026-09-09 19:51:52 -0300

    Renomeando a classe Program para retrieveData

--- src/application/retrieveData.java
+++ src/application/retrieveData.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Program {
+public class retrieveData {
 
     public static void main(String[] args){
 
