f8039b755aa8bcc7f164db26efd6cd92b1457895
Cristhiane Jacques <cjn_1707@hotmail.com>
2026-09-09 21:12:27 -0300

    Adicionando classe DbIntegrityException e implementando funcionalidade de exclusão de dados na classe deleteData

---
+++ src/application/deleteData.java
@@ -0,0 +1,43 @@
+package application;
+
+
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class deleteData {
+
+    public static void main(String[] args){
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "DELETE from department "
+                    + "WHERE "
+                    + "Id = ?");
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage()); // Lançando exceção personalizada
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();;
+        }
+
+
+    }
+
+
+}
